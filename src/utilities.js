57e7e4c6caefde620db9b58bcaafd48b8adc6cfb
Nick McMillan <nick.a.mcmillan@gmail.com>
2017-05-23 19:35:37 +1000

    single lined func

--- src/utilities.js
+++ src/utilities.js
@@ -26,6 +26,4 @@ export const closest = (el, fn) => {
     )
 }
 
-export const random = (min, max) => {
-    return parseInt( Math.random() * (max - min) + min, 10);
-}
+export const random = (min, max) => parseInt( Math.random() * (max - min) + min, 10)
