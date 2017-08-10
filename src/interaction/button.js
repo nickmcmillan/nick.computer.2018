61bab26b9a112a36c50ccbcd280293c38e5bbd56
Nick McMillan <nick.a.mcmillan@gmail.com>
2017-08-10 11:27:04 +1000

    Merge branch 'master' of https://github.com/heynick/nick.computer

--- src/interaction/button.js
+++ src/interaction/button.js
@@ -1,20 +1,30 @@
 import globals from '../globals'
 import options from '../settings/options'
 import {addBall} from '../balls'
+import {startAnimatingRainbow, stopAnimatingRainbow} from '../rainbow'
+
+
+const open = () => {
+    globals.doc.body.classList.add('open')
+    startAnimatingRainbow()
+}
+
+const close = () => {
+    globals.doc.body.classList.remove('open')
+    stopAnimatingRainbow()
+}
 
 let x
 let button = globals.doc.getElementById('trigger')
 button.addEventListener('mousedown', e => x = e.pageX)
-button.addEventListener('mouseup',  e => { if (e.pageX === x) globals.doc.body.classList.add('open') })
+button.addEventListener('mouseup',  e => { if (e.pageX === x) open() })
 button.addEventListener('touchstart', e => x = e.pageX)
 button.addEventListener('touchend', e => {
-    if (e.changedTouches[0].pageX === x) {
-        globals.doc.body.classList.add('open')
-    }
+    if (e.changedTouches[0].pageX === x) open()
 })
 
-globals.doc.getElementById('close').addEventListener('click', () => globals.doc.body.classList.remove('open'))
-globals.doc.getElementById('close').addEventListener('touchend', () =>  globals.doc.body.classList.remove('open'))
+globals.doc.getElementById('close').addEventListener('click', () => close())
+globals.doc.getElementById('close').addEventListener('touchend', () =>  close())
 
 globals.doc.onkeydown = () => {
     addBall(options.BALL_COUNT + 1)
---
+++ src/rainbow.js
@@ -0,0 +1,56 @@
+/* globals document, window */
+// https://codepen.io/hey-nick/pen/kkboWO
+
+import globals from './globals'
+
+const c = document.getElementById('bg')
+const $ = c.getContext('2d')
+const width = c.width
+const height = c.height
+let t = 0
+
+const contrast = 64
+const brightness = 192
+const framerate = 0.15
+
+
+const col = function(x, y, r, g, b) {
+    $.fillStyle = "rgb(" + r + "," + g + "," + b + ")"
+    $.fillRect(x, y, 1, 1)
+}
+const R = function(x, y, t) {
+    return( Math.floor(brightness + contrast * Math.cos( (x*x-y*y)/300 + t )) )
+}
+
+const G = function(x, y, t) {
+    return( Math.floor(brightness + contrast * Math.sin( (x*x*Math.cos(t/4)+y*y*Math.sin(t/3))/300 ) ) )
+}
+
+const B = function(x, y, t) {
+    return( Math.floor(brightness + contrast * Math.sin( 5*Math.sin(t/9) + ((x-100)*(x-100)+(y-100)*(y-100))/1100) ))
+}
+
+const renderLoop = function() {
+    for (let x = 0; x <= width; x++) {
+        for (let y = 0; y <= height; y++) {
+            col(x, y, R(x,y,t), G(x,y,t), B(x,y,t))
+        }
+    }
+    t = t + framerate
+
+    globals.animatingRainbow = window.requestAnimationFrame(renderLoop)
+}
+
+
+export const startAnimatingRainbow = () => {
+    if (!globals.animatingRainbow) {
+        renderLoop()
+    }
+}
+
+export const stopAnimatingRainbow = () => {
+    if (globals.animatingRainbow) {
+        window.cancelAnimationFrame(globals.animatingRainbow)
+        globals.animatingRainbow = undefined
+    }
+}
