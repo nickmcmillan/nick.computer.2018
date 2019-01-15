d99ae3651fcd9c69fc32fdff51821cb2ac768b8a
Nick McMillan <nick.a.mcmillan@gmail.com>
2019-01-15 21:42:34 +0100

    💄 tweaking options

--- src/settings/options.js
+++ src/settings/options.js
@@ -1,12 +1,12 @@
 import globals from '../globals';
 
 export default {
-  ROUGHNESS: 0.85, // 1:== perfect circle
-  SPREAD_PUSH: 0.7, // how hard the balls push against each other. 1:== neutral
-  MIN_SIZE: globals.w < 1024 ? 100 : globals.w < 1600 ? 140 : 240, // phone, tablet, big fella
-  MAX_SIZE: globals.w < 1024 ? 200 : globals.w < 1600 ? 250 : 400,
+  ROUGHNESS: 0.75, // 1:== perfect circle
+  SPREAD_PUSH: 0.15, // how hard the balls push against each other. 1:== neutral
   SPREAD_SPEED: 0.002, // how fast react to each other
-  BALL_COUNT: globals.w < 1024 ? 8 : globals.w < 1600 ? 10 : 12,
+  MIN_SIZE: globals.w < 568 ? 40 : globals.w < 1600 ? 80 : 200, // phone, tablet, big fella
+  MAX_SIZE: globals.w < 568 ? 200 : globals.w < 1600 ? 200 : 400,
+  BALL_COUNT: globals.w < 568 ? 8 : globals.w < 1600 ? 12 : 14,
   FRICTION: 0.95, // for inertia
   topBound: 0,
   leftBound: 0,
